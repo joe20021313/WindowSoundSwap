509834ef06e03f978550eac20ed4fa019c155550
Joe <131233284+joe20021313@users.noreply.github.com>
2024-04-08 05:38:13 +0100

    d

--- main.cpp
+++ main.cpp
@@ -6,18 +6,24 @@
 using namespace std;
 
 BOOL CALLBACK WindowProc(HWND hwnd, LPARAM lParam)
-{  RECT rect;
+{
+    RECT rect;
     if (GetWindowRect(hwnd, &rect))
     {
         wchar_t windowTitle[256];
         GetWindowTextW(hwnd, windowTitle, 256);
-        wcout << L"name: " << windowTitle << L", Window handle: " << hwnd << L", Rect: " << rect.left << L", " << rect.top << L", " << rect.right << L", " << rect.bottom << endl;
+
+        // Check if the window is not in the system tray
+        if (wcslen(windowTitle) > 0 && !(GetWindowLong(hwnd, GWL_EXSTYLE) & WS_EX_TOOLWINDOW) && rect.left >0 && rect.right  > 0 && rect.top >0 && rect.bottom >0 )
+        {
+            wcout << L"name: " << windowTitle << L", Window handle: " << hwnd << L", Rect: " << rect.left << L", " << rect.top << L", " << rect.right << L", " << rect.bottom << endl;
+        }
     }
     return TRUE;
 }
 int main()
 {
-   EnumWindows(WindowProc, 0);
+    EnumWindows(WindowProc, 0);
 
-    return  0;
+    return 0;
 }
